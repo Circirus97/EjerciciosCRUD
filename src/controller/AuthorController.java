bb6e986982b1dfde76bd46ff7980d23bbf35f5c2
Circirus97 <msuarezalejandra@gmail.com>
2024-03-20 18:47:56 -0500

    guardar cambios

--- src/controller/AuthorController.java
+++ src/controller/AuthorController.java
@@ -3,4 +3,6 @@ package controller;
 public class AuthorController {
 
 
+
+
 }
--- src/model/AuthorModel.java
+++ src/model/AuthorModel.java
@@ -1,4 +1,41 @@
 package model;
 
-public class AuthorModel {
+import database.AuthorCRUD;
+import entity.Author;
+
+import java.util.List;
+
+public class AuthorModel implements AuthorCRUD {
+
+
+    @Override
+    public Object insertAuthor(Object object) {
+
+        return null;
+    }
+
+    @Override
+    public Object updateAuthor(Object object) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuthor(Object object) {
+
+    }
+
+    @Override
+    public List<Object> findAllAuthor() {
+        return null;
+    }
+
+    @Override
+    public Object findByIdAuthor(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Author> findByNameAuthor(String name) {
+        return null;
+    }
 }
